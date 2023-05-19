723c878fa2a8ef1137177729b51be88cf6fcb041
Hridoy-shill <ridoyaaa60@gmail.com>
2023-05-19 22:39:28 +0600

    alltoys component updated

--- src/Pages/AllToysPage/AllToysPage.jsx
+++ src/Pages/AllToysPage/AllToysPage.jsx
@@ -8,7 +8,7 @@ const AllToysPage = () => {
     return (
         <>
             <h2 className='font-bold text-3xl border-b-2 border-black w-fit p-4 mx-auto'>All Car's</h2>
-            <div className='grid grid-cols-4 gap-3 py-20'>
+            <div className='grid grid-cols-4 gap-3 py-10'>
 
                 {
                     allToys.map(toy => <SingleToy key={toy._id} toy={toy}></SingleToy>)
